87cdc7a5744f50779d6f8b738cf261b20e80c079
stephant98 <stephantruchsess@gmail.com>
2020-12-12 16:51:10 +0000

    tinkering with js

---
+++ tinkering.js
@@ -0,0 +1,14 @@
+function multiply(a, b) {
+  return a * b
+}
+const result = multiply(2, 4);
+
+console.log(result);
+
+function hello(stephan, christian) {
+  return stephan * christian
+}
+
+const ageMultiplication = hello(22, 25)
+
+console.log(ageMultiplication);
